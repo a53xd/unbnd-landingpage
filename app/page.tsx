27be3335809838f8ab48b70482afe9f4646bd41c
stella <founder@petaltech.xyz>
2024-09-12 19:03:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Hey, we're unbound. The new{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              social internet
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Independent. And Non-Profit. For the better.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
