3b06fd453f577de8f207a42949b36d2ea8c3a041
stella <founder@petaltech.xyz>
2024-09-13 21:24:53 +0300

    sizies

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="https://i.ibb.co/R6j0Bqj/Butterfly.png"
           alt="Unbound logo"
-          width={28}
-          height={28}
+          width={34}
+          height={34}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
